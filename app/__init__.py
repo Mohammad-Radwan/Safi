98fec0682c5b14bf251addec50efd0fcb6ddd474
Mohamed Radwan <1mohammed.radwan@gmail.com>
2025-12-06 22:05:36 +0200

    feat: implement authentication service and controller with user registration and login endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 from app.logging_config import configure_logging
 from config import DevelopmentConfig
+
 from .routes.auth import auth_bp
 from .routes.groups import groups_bp
 from .routes.users import users_bp
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
 
     # register blueprints
     app.register_blueprint(users_bp)
